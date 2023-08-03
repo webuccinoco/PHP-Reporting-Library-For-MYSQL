471f810deec353a1384f835a78dd513c23df5346
Webuccino Products <139923924+webuccinoco@users.noreply.github.com>
2023-08-03 05:35:19 +0300

    Add files via upload

---
+++ sre_bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+require_once(__DIR__."/sre_config/config.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/ErrorMessages.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/constants.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/Helper.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/Filter.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/ReportPath.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/Engine.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/CustomEngine.php");
+require_once(__DIR__."/SmartReportingEngine/src/Engine/ReportOptions.php");
+
+
